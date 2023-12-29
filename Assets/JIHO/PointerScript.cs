f10c84fd8764caaa2f436a2f73b30dbe453028e4
jjjiiihhhooo <thdwlgh950@naver.com>
2023-12-29 20:16:06 +0900

    1

---
+++ Assets/JIHO/PointerScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class PointerScript : MonoBehaviour, IPointerClickHandler, IDragHandler
+{
+    public UnityEvent _event;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+    }
+
+
+}
---
+++ Assets/JIHO/UIManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Image timer_fill;
+
+
+    public void UpdateImage(float time, float maxTime)
+    {
+        timer_fill.fillAmount = time / maxTime;
+    }
+}
